5b031fcedff22c75f8f6342a4f1f0210241ed874
xstreca1 <martin.strecansky@gmail.com>
2014-10-17 14:14:46 +0200

    ServiceCheckDAO class finished - version1

--- src/main/java/cz/muni/fi/pa165/carmanagementsystem/ServiceCheckDAO.java
+++ src/main/java/cz/muni/fi/pa165/carmanagementsystem/ServiceCheckDAO.java
@@ -12,11 +12,46 @@ import java.util.List;
  * @author Martin Strecansky
  */
 public interface ServiceCheckDAO {
-    
-    public void insertServiceCheck();
-    public void updateServiceCheck(int scID);
-    public void deleteServiceCheck(int scID);
+
+    /**
+     * Method used to insert instance of ServiceCheck class to database
+     *
+     * @param seviceCheck
+     */
+    public void createServiceCheck(ServiceCheck seviceCheck);
+
+    /**
+     * Method used to get the instance of ServiceCheck class from database
+     * according to its ID, update this instance and save this updated
+     * serviceCheck to database
+     *
+     * @param seviceCheck
+     */
+    public void updateServiceCheck(ServiceCheck seviceCheck);
+
+    /**
+     * Method used to delete instance of ServiceCheck class from database
+     * according to its ID
+     *
+     * @param seviceCheck
+     */
+    public void deleteServiceCheck(ServiceCheck seviceCheck);
+
+    /**
+     * Method used to get number of days remaining to next service check
+     * according to its ID
+     *
+     * @param scID
+     * @return number of days
+     */
     public int getDaysToNext(int scID);
-    public List getServiceChecksForCar(Car car);    
-    
+
+    /**
+     * Method used to get all service checks assigned to some car
+     *
+     * @param car
+     * @return List of service checks
+     */
+    public List getServiceChecksForCar(Car car);
+
 }
