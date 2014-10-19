2aa66522ccc78f4c25e9f8fce5f771745f753a15
xstreca1 <martin.strecansky@gmail.com>
2014-10-19 17:51:09 +0200

    PersonDAOImplTest - first two methods

---
+++ src/test/java/cz/muni/fi/pa165/carmanagementsystem/PersonDAOImplTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.carmanagementsystem;
+
+import cz.muni.fi.pa165.carmanagementsystem.DAO.PersonDAOImpl;
+import cz.muni.fi.pa165.carmanagementsystem.Entities.Person;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Martin Strecansky
+ */
+public class PersonDAOImplTest {
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testInsertPerson() {
+        // create correct person
+        Person person = new Person();
+        // create new PersonDAO
+        PersonDAOImpl dao = new PersonDAOImpl();
+        // persist this person
+        dao.insertPerson(person);
+        // get this person from database
+        Person person1 = dao.getPersonByID(person.getId());
+        // person and person1 should be the same
+        assertEquals(person, person1);
+        
+    }
+    @Test
+    public void testUpdatePerson(){
+        // create correct person
+        Person person = new Person();
+        person.setName("JOHN");
+        // create new PersonDAO
+        PersonDAOImpl dao = new PersonDAOImpl();
+        // persist this person
+        dao.insertPerson(person);
+        // create updated person
+        Person person1 = new Person();
+        person1.setName("JOHNY");
+        // update person
+        dao.updatePerson(person1, person.getId());
+        // name of person should be JOHNY now
+        assertEquals(person.getName(), "JOHNY");        
+        
+    }
+}
