9ac2af4149f37c346ccbee2240d30fd24340259e
Petr Potucek <potucek.petr@gmail.com>
2014-10-20 08:04:50 +0200

    Added methods

--- src/main/java/cz/muni/fi/pa165/carmanagementsystem/LeaseDAO.java
+++ src/main/java/cz/muni/fi/pa165/carmanagementsystem/LeaseDAO.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.carmanagementsystem;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  *
- * @author jrumanov
+ * @author Petr Potucek
  */
 public interface LeaseDAO {
+    
+    public void createLease();
+    public void updateLease(int leaseId);
+    public void deleteLease(int leaseId);
+    public List getLeasesByPerson(String personId);
+    public List getAllLeases(Date from, Date until);    
     
 }
