914e31cf6b389ad6df163feda8bbbe1bc9773d8d
Petr Potucek <potucek.petr@gmail.com>
2014-10-20 08:05:21 +0200

    Added implementation, some things todo

--- src/main/java/cz/muni/fi/pa165/carmanagementsystem/Lease.java
+++ src/main/java/cz/muni/fi/pa165/carmanagementsystem/Lease.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This claas represents a lease that can any employee make.
+ * Employee is able to lease a car according to his status.
+ * Every employee's travel is recorded (where and when, number of kilometers..)
+ * Every lease has date from and to which the lease is planned.
+ * Every lease need to be approved first.
  */
 package cz.muni.fi.pa165.carmanagementsystem;
 
@@ -13,26 +15,132 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
- *
+ * Masaryk university project
+ * 
  * @author Petr Potucek
  */
 @Entity
 public class Lease implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int leaseId;
+    
+    @Column(nullable = false)
     private int carMileage;
+    
+    @Temporal(TemporalType.DATE)
     private Date dateOfLease;
+    
+    @Temporal(TemporalType.DATE)
     private Date dateOfReturn;
+    
+    @Column(nullable = false)
     private String vehicleRegPlate;
+    
+    @Column(nullable = false)
     private Boolean isClosed;
     
+    @ManyToOne
     private Car car;
+    
+    @ManyToOne
     private Person person;
     
-    private enum ReturnedStatus{OK, BROKEN};//TODO
-    private enum TravelReason{PERSONAL, WORK};
+    @Enumerated(EnumType.STRING)
+    private ReturnedStatus returnedStatus;//TODO
+    
+    public enum ReturnedStatus {
+        BROKEN,
+        READY,
+        EMPTYFUEL        
+    };
+    
+    @Enumerated(EnumType.STRING)
+    private TravelReason travelReason;
+    
+    public enum TravelReason {
+        PERSONAL,
+        WORK        
+    };
+
+
+    /**
+     * @return the returnedStatus
+     */
+    public ReturnedStatus getReturnedStatus() {
+        return returnedStatus;
+    }
+
+    /**
+     * @return the travelReason
+     */
+    public TravelReason getTravelReason() {
+        return travelReason;
+    }
+
+    /**
+     * @param carMileage the carMileage to set
+     */
+    public void setCarMileage(int carMileage) {
+        this.carMileage = carMileage;
+    }
+
+    /**
+     * @param dateOfLease the dateOfLease to set
+     */
+    public void setDateOfLease(Date dateOfLease) {
+        this.dateOfLease = dateOfLease;
+    }
+
+    /**
+     * @param dateOfReturn the dateOfReturn to set
+     */
+    public void setDateOfReturn(Date dateOfReturn) {
+        this.dateOfReturn = dateOfReturn;
+    }
+
+    /**
+     * @param vehicleRegPlate the vehicleRegPlate to set
+     */
+    public void setVehicleRegPlate(String vehicleRegPlate) {
+        this.vehicleRegPlate = vehicleRegPlate;
+    }
+
+    /**
+     * @param isClosed the isClosed to set
+     */
+    public void setIsClosed(Boolean isClosed) {
+        this.isClosed = isClosed;
+    }
+
+    /**
+     * @param car the car to set
+     */
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    /**
+     * @param person the person to set
+     */
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    /**
+     * @param returnedStatus the returnedStatus to set
+     */
+    public void setReturnedStatus(ReturnedStatus returnedStatus) {
+        this.returnedStatus = returnedStatus;
+    }
+
+    /**
+     * @param travelReason the travelReason to set
+     */
+    public void setTravelReason(TravelReason travelReason) {
+        this.travelReason = travelReason;
+    }
 
     /**
      * @return the leaseId
@@ -111,7 +219,7 @@ public class Lease implements Serializable {
             return false;
         }
         Lease other = (Lease) object;
-        if (this.id != other.id) {
+        if (this.leaseId != other.leaseId) {
             return false;
         }
         return true;
