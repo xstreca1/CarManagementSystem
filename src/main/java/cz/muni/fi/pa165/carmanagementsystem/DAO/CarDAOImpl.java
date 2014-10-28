5d65a7108e1b8643809ef8c37ab51ca64b9ce691
xpuchly <jpuchly@gmail.com>
2014-10-28 15:06:24 +0100

    CarDAOImpl repair

--- src/main/java/cz/muni/fi/pa165/carmanagementsystem/DAO/CarDAOImpl.java
+++ src/main/java/cz/muni/fi/pa165/carmanagementsystem/DAO/CarDAOImpl.java
@@ -6,7 +6,8 @@
 package cz.muni.fi.pa165.carmanagementsystem.DAO;
 
 import cz.muni.fi.pa165.carmanagementsystem.Entities.Car;
-import cz.muni.fi.pa165.carmanagementsystem.Entities.Person;
+import cz.muni.fi.pa165.carmanagementsystem.Entities.Car.bodyStyle;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -39,40 +40,46 @@ public class CarDAOImpl implements CarDAO {
     }
 
     @Override
-    public void updateCar(Car car, String vehicleRegPlate) {
+    public void updateCar(Car car, Integer carID) {
 
-        // create new EntityManager
+       //create Entity Manager
+        //EntityManagerFactory emf = Persistence.createEntityManagerFactory("carManagementSystem-unit");
         EntityManager em = emf.createEntityManager();
+
+        //begin of a transaction
         em.getTransaction().begin();
 
-        // get instance of Car according to its vehicle registration plate. Save this instance to variable "update"
-        String query = "SELECT * FROM Car car WHERE vehicleRegPlate='" + vehicleRegPlate + "'";
-        Car update = em.createQuery(query, Car.class).getSingleResult();
+        //get all updatable attributes from the updated car entity instance
+        Integer mileage = car.getMileage();
+        Boolean availibility = car.isAvailibility();
+        
+        //actual query
+        String sql = "UPDATE Car c SET c.mileage = :mileage, "
+                + "c.availibility = :availibility";
+        
+        em.createQuery(sql).setParameter("mileage", mileage)
+                .setParameter("availibility", availibility)
+                .executeUpdate();
 
-        // get new values of attributes
-        int mileage = car.getMileage();
-        boolean availibility = car.isAvailibility();
-
-        // replace actual values with new values
-        update.setMileage(mileage);
-        update.setAvailibility(availibility);
-
-        // save updated serviceCheck to database
-        em.persist(update);
+        //commiting changes and closing entity manager
         em.getTransaction().commit();
         em.close();
     }
 
     @Override
-    public void deleteCar(String vehicleRegPlate) {
+    public void deleteCar(Integer personID) {
         // create new EntityManager
         EntityManager em = emf.createEntityManager();
+        
         em.getTransaction().begin();
 
-        // delete serviceCheck from database according to its ID
-        String query = "DELETE * FROM Car WHERE vehicleRegPlate='" + vehicleRegPlate + "'";
-        em.createQuery(query).executeUpdate();
+        // delete car from DB according to its ID
+        Car car = getCarByID(personID);
+        
+        em.remove(car);
+        
         em.getTransaction().commit();
+        
         em.close();
     }
 
@@ -80,10 +87,13 @@ public class CarDAOImpl implements CarDAO {
     public List listAllAvailableCars() {
         // create new EntityManager
         EntityManager em = emf.createEntityManager();
+        
+        //begin of a transaction
+        em.getTransaction().begin();
 
         // get all cars that are available. save them to list
-        String query = "SELECT * FROM Car where isAvailibility=true";
-        List<Car> cars = em.createQuery(query).getResultList();
+        String query = "SELECT c FROM Car c where isAvailibility=true";
+        List<Car> cars = em.createQuery(query,Car.class).getResultList();
 
         // cloese EntityManager
         em.getTransaction().commit();
@@ -96,10 +106,14 @@ public class CarDAOImpl implements CarDAO {
     public List getCarByCategory(int category) {
         // create new EntityManager
         EntityManager em = emf.createEntityManager();
+        
+        //begin of a transaction
+        em.getTransaction().begin();
 
         // get all cars that have the chosen category. save them to list
-        String query = "SELECT * FROM Car where category='" + category + "'";
-        List<Car> cars = em.createQuery(query).getResultList();
+        String query = "SELECT c FROM Car c where category=:car.Category";
+        List<Car> cars = em.createQuery(query, Car.class).
+                setParameter("car.Category", category).getResultList();
 
         // cloese EntityManager
         em.getTransaction().commit();
@@ -112,10 +126,14 @@ public class CarDAOImpl implements CarDAO {
     public List getCarBySeats(int seats) {
         // create new EntityManager
         EntityManager em = emf.createEntityManager();
+        
+         //begin of a transaction
+        em.getTransaction().begin();
 
         // get all cars that have the chosen number of seats. save them to list
-        String query = "SELECT * FROM Car where numberOfSeats='" + seats + "'";
-        List<Car> cars = em.createQuery(query).getResultList();
+        String query = "SELECT c FROM Car c where numberOfSeats=:carNumberOfSeats";
+        List<Car> cars = em.createQuery(query, Car.class).
+                setParameter("carNumberOfSeats", seats).getResultList();
 
         // cloese EntityManager
         em.getTransaction().commit();
@@ -128,10 +146,14 @@ public class CarDAOImpl implements CarDAO {
     public List getCarByBodyStyle(Car.bodyStyle bs) {
         // create new EntityManager
         EntityManager em = emf.createEntityManager();
+        
+        //begin of a transaction
+        em.getTransaction().begin();
 
         // get all cars that have the chosen body style. save them to list
-        String query = "SELECT * FROM Car where bodyStyle='" + bs + "'";
-        List<Car> cars = em.createQuery(query).getResultList();
+        String query = "SELECT c FROM Car c where c.bodyStyle=:carBodyStyle";
+         List<Car> cars = em.createQuery(query, Car.class).
+                setParameter("carBodyStyle", bs).getResultList();
 
         // cloese EntityManager
         em.getTransaction().commit();
@@ -140,7 +162,7 @@ public class CarDAOImpl implements CarDAO {
         return cars;
     }
 
-    public Person getCarByID(Integer CarID) {
+    public Car getCarByID(Integer CarID) {
 
         //create Entity Manager
         //EntityManagerFactory emf = Persistence.createEntityManagerFactory("carManagementSystem-unit");
@@ -151,13 +173,13 @@ public class CarDAOImpl implements CarDAO {
 
         //actual query
         String sql = "SELECT c FROM Car c WHERE c.id=:carID";
-        Person person = em.createQuery(sql, Person.class)
+        Car car = em.createQuery(sql, Car.class)
                 .setParameter("carID", CarID).getResultList().get(0);
 
         //commiting changes and closing entity manager
         em.getTransaction().commit();
         em.close();
 
-        return person;
+        return car;
     }
 }
