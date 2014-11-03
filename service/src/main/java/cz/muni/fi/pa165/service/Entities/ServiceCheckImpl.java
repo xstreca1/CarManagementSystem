910da064d110b632640da3a17eb9c2f87952bee9
xstreca1 <martin.strecansky@gmail.com>
2014-11-03 19:56:03 +0100

    ServiceCheckImpl (service layer) - cretae method implemented

--- service/src/main/java/cz/muni/fi/pa165/service/Entities/ServiceCheckImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/Entities/ServiceCheckImpl.java
@@ -4,10 +4,13 @@
  */
 package cz.muni.fi.pa165.service.Entities;
 
+import cz.muni.fi.pa165.persistence.DAO.ServiceCheckDAO;
 import cz.muni.fi.pa165.persistence.Entities.Car;
 import cz.muni.fi.pa165.persistence.Entities.ServiceCheck;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,30 +18,33 @@ import org.springframework.stereotype.Service;
  * @author Petr Potucek
  */
 @Service
+@Transactional
 public class ServiceCheckImpl implements ServiceCheckInterface {
-
-    public List<ServiceCheck> getChecksForCar(Car car) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    // create ServiceCheck DAO
+    private ServiceCheckDAO scDAO;
+    
+    // setter for ServiceCheck DAO  
+    public void setDao(ServiceCheckDAO scDAO) {
+        this.scDAO = scDAO;
     }
 
-    public void createNewCheck(ServiceCheck.ServiceCheckName name, String description, Car car, int serviceInterval, Date lastCheck) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void createServiceCheck(ServiceCheck.ServiceCheckName name, String description, Car car, int serviceInterval){
+       // create instance of calendar to find out current time which will be used as paramter fo lastCheck
+       Calendar now = Calendar.getInstance();       
+       
+       // create new serviceCheck, set lastCheck as current date
+       ServiceCheck check = new ServiceCheck();
+       check.setName(name);
+       check.setDescription(description);
+       check.setCar(car);
+       check.setServiceInterval(serviceInterval);
+       check.setLastCheck(now.getTime());
+       
+       // save to database using some implementation od DAO
+       scDAO.createServiceCheck(check);
+        
     }
 
-    public void setDescription(Integer scID, String description) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void deleteServiceCheck(Integer ScID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void showDaysToNextServiceCheck(Integer ScID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void changeNameOfCheck(Integer ScID, ServiceCheck.ServiceCheckName name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
     
 }
