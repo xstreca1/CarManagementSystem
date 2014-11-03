662a623eb25ad70a2344273ecfe5c91d9e8bdd4c
Petr Potucek <potucek.petr@gmail.com>
2014-11-03 15:58:18 +0100

    Created claas for testing service check

---
+++ service/src/test/java/cz/muni/fi/pa165/Tests/ServiceCheckTest.java
@@ -0,0 +1,33 @@
+package cz.muni.fi.pa165.Tests;
+
+
+/**
+ * Tests using Mock DAO objects
+ * 
+ * @author Petr Potucek
+ */
+public class ServiceCheckTest {
+
+    @Test 
+    public void testGetChecksForCar() {}
+    
+    @Test 
+    public void testCreateNewCheck() {}
+    
+    @Test 
+    public void testDescriptionSet() {}
+    
+    @Test 
+    public void deleteTest() {}
+    
+    @Test 
+    public void testNameChange() {}
+    
+    @Test 
+    public void testGetDaysToNext() {}
+    
+
+}
+
+    
+
