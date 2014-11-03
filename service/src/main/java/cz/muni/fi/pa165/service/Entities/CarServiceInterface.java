13f6ded3cb85705a45a7a3d4a74fbd3347bac2fb
xpuchly <jpuchly@gmail.com>
2014-11-03 20:18:36 +0100

    CarServiceInterface added

---
+++ service/src/main/java/cz/muni/fi/pa165/service/Entities/CarServiceInterface.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pa165.service.Entities;
+
+import cz.muni.fi.pa165.persistence.Entities.Car;
+import cz.muni.fi.pa165.persistence.Entities.Lease;
+import cz.muni.fi.pa165.persistence.Entities.Person;
+import java.util.List;
+
+/**
+ *
+ * @author jozef.puchly
+ */
+public interface CarServiceInterface {
+    
+    public void createCar(Car car);
+    
+    public void setAvailibility(boolean availibility);
+    
+    public Lease leaseCar(Person person, Car car);
+    
+    public Car getCarInfo(Integer carID);
+    
+    public void returnCar(Lease lease);
+    
+    public List<Car> findAllCars(boolean alsoInactive);
+    
+    public void retireCar(Car car);
+}
