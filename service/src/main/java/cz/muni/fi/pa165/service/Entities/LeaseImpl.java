0b3036d49c2dc23be1956e57f39734872239103d
Petr Potucek <potucek.petr@gmail.com>
2014-11-04 09:38:11 +0100

    Implementation added

--- service/src/main/java/cz/muni/fi/pa165/service/Entities/LeaseImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/Entities/LeaseImpl.java
@@ -4,40 +4,77 @@
  */
 package cz.muni.fi.pa165.service.Entities;
 
+import cz.muni.fi.pa165.persistence.DAO.LeaseDAO;
 import cz.muni.fi.pa165.persistence.Entities.Car;
 import cz.muni.fi.pa165.persistence.Entities.Lease;
+import cz.muni.fi.pa165.persistence.Entities.Lease.ReturnedStatus;
 import cz.muni.fi.pa165.persistence.Entities.Person;
 import java.util.Date;
 import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Petr Potucek
  */
+@Service
+@Transactional
 public class LeaseImpl implements LeaseInterface {
-
-    public void createLease(Car car, Person person, Lease.TravelReason travelReason, int carMileage, Date dateOfLease, Date dateOfReturn) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    private LeaseDAO leaseDao;
+    
+    /**
+     * setter for lease 
+     * 
+     * @param leaseDao lease to be setted
+     */
+    public void setLeaseDao(LeaseDAO leaseDao){
+        this.leaseDao = leaseDao;
     }
 
-    public void setReturnedStatus(int id, Lease.ReturnedStatus status) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void createLease(Car car, Person person, Lease.TravelReason travelReason, int carMileage, Date dateOfLease, Date dateOfReturn) {
+        //creation of lease to persist
+        Lease lease = new Lease();
+        
+        //set parameters to lease before persist
+        lease.setCar(car);
+        lease.setPerson(person);
+        lease.setTravelReason(travelReason);
+        lease.setCarMileage(carMileage);
+        lease.setDateOfLease(dateOfLease);
+        lease.setDateOfReturn(dateOfReturn);
+        
+        //set if lease is closed
+        lease.setIsClosed(Boolean.FALSE);
+        
+        //persist
+        leaseDao.createLease(lease);
+    }
+
+    public void setReturnedStatus(int id, ReturnedStatus status) {
+        (leaseDao.getLeaseByID(id)).setReturnedStatus(status);
     }
 
     public List getLeaseByPerson(Person person) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Lease> leasesForPerson;
+        
+        return leasesForPerson = leaseDao.getLeasesByPerson(person);      
     }
 
-    public List getAllLeases() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List getAllLeases(Date from, Date to) {
+        List<Lease> allLeases;
+        
+        return allLeases = leaseDao.getAllLeases(from,to);
+        
     }
 
     public Lease getLeaseById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return leaseDao.getLeaseByID(id);
     }
 
-    public void deleteLease(int leaseId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void deleteLease(int leaseId) {    
+        leaseDao.deleteLease(leaseId);
     }
     
 }
