d9e6719e475c88667e74e548d8d4156b31fc6154
Petr Potucek <potucek.petr@gmail.com>
2014-11-04 10:34:36 +0100

    Service check test claas added, without implementation

---
+++ service/src/test/java/cz/muni/fi/pa165/Tests/ServiceCheckImplTest.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.pa165.Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Service check service test file
+ * 
+ * @author Petr Potucek
+ */
+public class ServiceCheckImplTest {
+    
+    @Before
+    public void setUp(){
+    }
+    
+    @After
+    public void tearDown(){
+    }
+    
+    @Test
+    public void testCreateServiceCheck(){
+    }
+    
+    @Test
+    public void testGetDaysToNextServiceCheck(){
+    }
+    
+    @Test
+    public void testSetCheckInterval(){
+    }
+    
+    @Test
+    public void testGetServiceChecksForCar(){
+    }    
+}
