90068f74671f08e02b8867c72bc90f186e99d5fb
xpuchly <jpuchly@gmail.com>
2014-11-05 11:11:05 +0100

    Car service test added + mockito dependency added

---
+++ service/src/test/java/cz/muni/fi/pa165/Tests/CarImplTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pa165.Tests;
+
+import cz.muni.fi.pa165.persistence.Entities.Car;
+import cz.muni.fi.pa165.persistence.dao.CarDAO;
+import cz.muni.fi.pa165.service.Entities.CarImpl;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+/**
+ *
+ * @author jozef.puchly
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CarImplTest {
+    
+    @Mock
+    CarDAO mockCarDao;
+    
+    @Mock
+    Car car;
+    
+    @Mock
+    List<Car> cars;
+    
+    @InjectMocks
+    private CarImpl service = new CarImpl();
+
+    @Before
+    public void setUp() {
+        
+    }
+
+		
+}
