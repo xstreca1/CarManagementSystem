b102e8410947eca6358be96b9d07d3ce2d90992e
xpuchly <jpuchly@gmail.com>
2014-11-06 09:40:10 +0100

    distance for carmileage changed

--- persistence/src/test/java/cz/muni/fi/pa165/persistence/tests/LeaseDAOImplTest.java
+++ persistence/src/test/java/cz/muni/fi/pa165/persistence/tests/LeaseDAOImplTest.java
@@ -5,14 +5,15 @@
  */
 package cz.muni.fi.pa165.persistence.tests;
 
-import cz.muni.fi.pa165.persistence.dao.LeaseDAOImpl;
 import cz.muni.fi.pa165.persistence.Entities.Address;
 import cz.muni.fi.pa165.persistence.Entities.Car;
 import cz.muni.fi.pa165.persistence.Entities.Lease;
 import cz.muni.fi.pa165.persistence.Entities.Person;
+import cz.muni.fi.pa165.persistence.dao.LeaseDAOImpl;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.Assert;
@@ -26,8 +27,8 @@ import org.junit.Test;
  */
 public class LeaseDAOImplTest {
 
-    private static LeaseDAOImpl dao
-            = new LeaseDAOImpl(Persistence.createEntityManagerFactory("carManagementSystem-unit"));
+    private EntityManager em;
+    private LeaseDAOImpl dao;
 
     private static Lease lease;
     private static Lease lease2;
@@ -43,8 +44,10 @@ public class LeaseDAOImplTest {
 
     @Before
     public void setUpClass() {
-        EntityManager em = dao.getEntityManagerFactory().createEntityManager();
-        em.getTransaction().begin();
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("carManagementSystem-unit");
+        em = emf.createEntityManager();
+        dao = new LeaseDAOImpl(em);
 
         date1 = new Date(2014, 1, 1);
         date2 = new Date(2014, 1, 2);
@@ -65,6 +68,7 @@ public class LeaseDAOImplTest {
         car1.setMileage(55000);
         car1.setNumberOfSeats(4);
         car1.setTransmission(true);
+        car1.setIsActive(true);
 
         car2 = new Car();
         car2.setAvailibility(false);
@@ -79,6 +83,7 @@ public class LeaseDAOImplTest {
         car2.setMileage(56000);
         car2.setNumberOfSeats(4);
         car2.setTransmission(true);
+        car2.setIsActive(true);
         
         em.persist(car1);
         em.persist(car2);
@@ -89,13 +94,13 @@ public class LeaseDAOImplTest {
         person.setAddress(new Address());
         person.setPosition("HR");
         person.setNationality("US");
-        person.setSalary(25_000);       
-        
+        person.setSalary(25_000);  
+       
         em.persist(person);
 
         //create lease - two of them       
         lease = new Lease();
-        lease.setCarMileage(car1.getMileage());
+        lease.setDistance(car1.getMileage());
         lease.setDateOfLease(date1);
         lease.setDateOfReturn(date2);
         lease.setIsClosed(true);
@@ -103,7 +108,7 @@ public class LeaseDAOImplTest {
         lease.setPerson(person);
 
         lease2 = new Lease();
-        lease2.setCarMileage(car1.getMileage());
+        lease2.setDistance(car1.getMileage());
         lease2.setDateOfLease(date3);
         lease2.setDateOfReturn(date4);
         lease2.setIsClosed(true);
@@ -120,14 +125,14 @@ public class LeaseDAOImplTest {
 
         //create DAO object
         toInsert = new Lease();
-        toInsert.setCarMileage(car1.getMileage());
+        toInsert.setDistance(car1.getMileage());
         toInsert.setDateOfLease(date3);
         toInsert.setDateOfReturn(date4);
         toInsert.setIsClosed(true);
         toInsert.setCar(car1);
 
         toUpdate = new Lease();
-        toUpdate.setCarMileage(car2.getMileage());
+        toUpdate.setDistance(car2.getMileage());
         toUpdate.setDateOfLease(date3);
         toUpdate.setDateOfReturn(date4);
         toUpdate.setIsClosed(false);
@@ -136,8 +141,6 @@ public class LeaseDAOImplTest {
 
     @After
     public void tearDownClass() {
-
-        EntityManager em = dao.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
         em.createQuery("DELETE FROM Lease").executeUpdate();
         em.getTransaction().commit();
@@ -148,24 +151,20 @@ public class LeaseDAOImplTest {
      */
     @Test
     public void testIsInDB() {
-        EntityManager em = dao.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
         List<Lease> leases = em.createQuery("SELECT l FROM Lease l", Lease.class).getResultList();
         assertEquals(leases.size(), 2);
         em.getTransaction().commit();
-        em.close();
     }
 
     @Test
     public void testCreateLease() {
         dao.createLease(toInsert);
 
-        EntityManager em = dao.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
         Lease lease3 = em.find(Lease.class, toInsert.getId());
         Assert.assertTrue(em.contains(lease3));
         em.getTransaction().commit();
-        em.close();
     }
 
     /**
@@ -174,16 +173,14 @@ public class LeaseDAOImplTest {
     @Test
     public void testUpdateLease() {
         dao.updateLease(toUpdate, lease2.getLeaseId());
-        EntityManager em = dao.getEntityManagerFactory().createEntityManager();
         em.getTransaction().begin();
         Lease updatedLease = em.find(Lease.class, lease2.getLeaseId());
         Assert.assertEquals(updatedLease.getCar(), toUpdate.getCar());
         Assert.assertEquals(updatedLease.getIsClosed(), toUpdate.getIsClosed());
         Assert.assertEquals(date3, toUpdate.getDateOfLease());
         Assert.assertEquals(updatedLease.getDateOfReturn(), toUpdate.getDateOfReturn());
-        Assert.assertEquals(updatedLease.getCarMileage(), toUpdate.getCarMileage());
+        Assert.assertEquals(updatedLease.getDistance(), toUpdate.getDistance());
         em.getTransaction().commit();
-        em.close();
     }
 
     /**
@@ -192,12 +189,10 @@ public class LeaseDAOImplTest {
     @Test
     public void testDeleteLease() {
         Integer id = lease.getLeaseId();
-        EntityManager em = dao.getEntityManagerFactory().createEntityManager();
         dao.deleteLease(lease.getLeaseId());
         em.getTransaction().begin();
         Assert.assertFalse(em.contains(lease));
         em.getTransaction().commit();
-        em.close();
     }
 
     /**
