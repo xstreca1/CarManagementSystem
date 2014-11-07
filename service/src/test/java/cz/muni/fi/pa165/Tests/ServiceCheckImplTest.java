446400492198d3cfa66151b06c8f6ed7b1d35323
xstreca1 <martin.strecansky@gmail.com>
2014-11-07 15:15:47 +0100

    ServiceCheckImplTest SetUp() added

--- service/src/test/java/cz/muni/fi/pa165/Tests/ServiceCheckImplTest.java
+++ service/src/test/java/cz/muni/fi/pa165/Tests/ServiceCheckImplTest.java
@@ -1,18 +1,44 @@
 package cz.muni.fi.pa165.Tests;
 
+import cz.muni.fi.pa165.persistence.DAO.ServiceCheckDAO;
+import cz.muni.fi.pa165.persistence.Entities.ServiceCheck;
+import cz.muni.fi.pa165.service.service.ServiceCheckImpl;
+import cz.muni.fi.pa165.service.service.ServiceCheckInterface;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 /**
  * Service check service test file
  * 
  * @author Petr Potucek
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceCheckImplTest {
+    
+    @Mock
+    ServiceCheckDAO mockCheckDAO;
+    
+    @InjectMocks
+    ServiceCheckInterface serviceCheckImpl = new ServiceCheckImpl();
     
     @Before
     public void setUp(){
+        Mockito.when(mockCheckDAO.createServiceCheck(Matchers.any(ServiceCheck.class))).thenAnswer(new Answer<ServiceCheck>() {
+            @Override
+            public ServiceCheck answer(InvocationOnMock inv) throws Throwable {
+                Object[] args = inv.getArguments();
+                return (ServiceCheck) args[0];
+            }
+        });
     }
     
     @After
@@ -21,6 +47,9 @@ public class ServiceCheckImplTest {
     
     @Test
     public void testCreateServiceCheck(){
+        
+        
+        
     }
     
     @Test
