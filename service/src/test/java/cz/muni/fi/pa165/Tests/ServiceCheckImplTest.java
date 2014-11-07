552f900acf14d2c69829b495e9a86f8cdfc91a67
jozef.puchly <jozef.puchly@JPUCHLY-LT.tul.solarwinds.net>
2014-11-07 17:55:20 +0100

    get service checks for car test added

--- service/src/test/java/cz/muni/fi/pa165/Tests/ServiceCheckImplTest.java
+++ service/src/test/java/cz/muni/fi/pa165/Tests/ServiceCheckImplTest.java
@@ -1,12 +1,20 @@
 package cz.muni.fi.pa165.Tests;
 
 import cz.muni.fi.pa165.persistence.DAO.ServiceCheckDAO;
+import cz.muni.fi.pa165.persistence.Entities.Car;
 import cz.muni.fi.pa165.persistence.Entities.ServiceCheck;
+import cz.muni.fi.pa165.service.dto.CarDTO;
 import cz.muni.fi.pa165.service.dto.ServiceCheckDTO;
 import cz.muni.fi.pa165.service.service.ServiceCheckImpl;
 import cz.muni.fi.pa165.service.service.ServiceCheckInterface;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
@@ -31,25 +39,40 @@ public class ServiceCheckImplTest {
     @Mock
     ServiceCheckDAO mockCheckDAO;
     
+    
     @Mock
     ServiceCheck serviceCheck;
     
     @Mock
     ServiceCheckDTO serviceCheckDTO;
     
+    @Mock
+    CarDTO carDto;
+    
     @InjectMocks
     ServiceCheckInterface serviceCheckImpl = new ServiceCheckImpl();
     
     @Before
     public void setUp(){
-        Mockito.when(mockCheckDAO.createServiceCheck(Matchers.any(ServiceCheck.class))).thenAnswer(new Answer<ServiceCheck>() {
+        Mockito.when(mockCheckDAO.createServiceCheck(Matchers.any(ServiceCheck.class)))
+                .thenAnswer(new Answer<ServiceCheck>() {
             @Override
             public ServiceCheck answer(InvocationOnMock inv) throws Throwable {
                 Object[] args = inv.getArguments();
                 return (ServiceCheck) args[0];
             }
         });
+        
+        List<ServiceCheck> allChecks = new ArrayList<>();
+		allChecks.add(serviceCheck);
+		allChecks.add(serviceCheck);
+        Mockito.when(mockCheckDAO.getServiceChecksForCar(Matchers.any(Car.class)))
+				.thenReturn(allChecks);
+            
+            
     }
+    
+                        
     
     @Test
     public void testCreateServiceCheck(){
@@ -73,5 +96,10 @@ public class ServiceCheckImplTest {
     
     @Test
     public void testGetServiceChecksForCar(){
+        
+        List<ServiceCheckDTO> list = serviceCheckImpl.getServiceChecksForCar(carDto);
+			assertNotNull(list);
+			assertEquals(2, list.size());
+        
     }    
 }
