023928058d7782bdc4b131ffe63152982a3b9d27
jozefpuchly <jozefpuchly@192.168.0.111>
2014-12-14 10:42:00 +0100

    rest client module added

---
+++ mavenproject1/rest_client/src/main/java/cz/muni/fi/pa165/rest_client/CarClient.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pa165.rest_client;
+
+/**
+ *CLI client
+ * 
+ * @author jozefpuchly
+ */
+public class CarClient {
+    
+    private static String BASIC_URL = "http://localhost:8080/pa165/rest/";
+    
+    private static final String COMMAND_MACHINE = "machine";
+    
+    
+    
+}
