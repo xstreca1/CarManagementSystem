5e0f62a372921e95477267cb547884de4dc8ad71
jozefpuchly <jozefpuchly@192.168.0.111>
2014-12-14 10:44:17 +0100

    car client added attributes

--- mavenproject1/rest_client/src/main/java/cz/muni/fi/pa165/rest_client/CarClient.java
+++ mavenproject1/rest_client/src/main/java/cz/muni/fi/pa165/rest_client/CarClient.java
@@ -7,9 +7,21 @@ package cz.muni.fi.pa165.rest_client;
  */
 public class CarClient {
     
-    private static String BASIC_URL = "http://localhost:8080/pa165/rest/";
+    private static final String FIND_ALL_OPERATION = "findAll";
+    private static final String ADD_OPERATION = "add";
+    private static final String UPDATE_OPERATION = "update";
+    private static final String DELETE_OPERATION = "delete";
+    private static final String FIND_BY_ID_OPERATION = "findbyid";
     
-    private static final String COMMAND_MACHINE = "machine";
+    private static final String CREATE_CAR = "http://localhost:8080/pa165/rest/car/add";
+    private static final String GET_CAR = "http://localhost:8080/pa165/rest/car/get?id=%d";
+    private static final String UPDATE_CAR = "http://localhost:8080/pa165/rest/car/update";
+    private static final String DELETE_CAR = "http://localhost:8080/pa165/rest/car/delete";
+    private static final String GET_ALL_CARS = "http://localhost:8080/pa165/rest/car/getAll";
+    
+    public CarClient(String[] args) {
+        
+    }
     
     
     
