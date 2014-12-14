f355d22dece37fd4724b36e90d7758d37370ff9c
jozefpuchly <jozefpuchly@192.168.0.111>
2014-12-14 11:47:37 +0100

    controllers created

---
+++ web/src/main/java/cz/muni/fi/pa165/rest/PersonRestController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.rest;
+
+import cz.muni.fi.pa165.service.service.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jozefpuchly
+ */
+@RestController
+@RequestMapping("/rest/person")
+public class PersonRestController {
+    
+    @Autowired
+    PersonServices service;
+    
+}
