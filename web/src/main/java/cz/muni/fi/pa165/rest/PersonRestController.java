effd1705a68886397cfafa0f886637059b8aa379
xrumanov <jakub.rumanovsky@gmail.com>
2014-12-15 10:24:37 +0100

    add REST controller for person service 	* found some problems that needs to be solved with xstreca1

--- web/src/main/java/cz/muni/fi/pa165/rest/PersonRestController.java
+++ web/src/main/java/cz/muni/fi/pa165/rest/PersonRestController.java
@@ -5,9 +5,15 @@
  */
 package cz.muni.fi.pa165.rest;
 
+import cz.muni.fi.pa165.rest_messages.ReceiveActivePeopleListMessage;
+import cz.muni.fi.pa165.rest_messages.ReceivePersonMessage;
+import cz.muni.fi.pa165.service.dto.PersonDTO;
 import cz.muni.fi.pa165.service.service.PersonServices;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,5 +26,107 @@ public class PersonRestController {
     
     @Autowired
     PersonServices service;
+    @RequestMapping("/get")
+    public ReceivePersonMessage getCarById(@RequestParam(required = true) int id)
+    {
+        PersonDTO person;
+        ReceivePersonMessage result = new ReceivePersonMessage();
+        
+        try
+        {
+            person = service.getPersonByID(id);
+            result.setObject(person);
+        }
+        catch(Exception ex)
+        {
+            result.setMessage(ex.getMessage());
+            result.setObject(null);
+        }
+        
+        return result;
+    }
     
+    @RequestMapping("/getAll")
+    public ReceiveActivePeopleListMessage getAllPeople()
+    {
+        ReceiveActivePeopleListMessage result = new ReceiveActivePeopleListMessage();
+        
+        try
+        {
+            List<PersonDTO> list = service.findAllPeople(true);
+            result.setList(list);
+        }
+        catch(Exception ex)
+        {
+            result.setMessage(ex.getMessage());
+            result.setList(null);
+        }
+        
+        return result;
+    }
+    
+    @RequestMapping("/update")
+    public ReceivePersonMessage updateCar(@RequestBody PersonDTO person)
+    {
+      
+        ReceivePersonMessage result = new ReceivePersonMessage();
+        
+        try
+        {
+            Integer id = person.getId();
+            service.editPerson(person, id);
+            result.setObject(person);
+            
+        }
+        catch(Exception ex)
+        {
+            result.setMessage(ex.getMessage());
+            result.setObject(null);
+        }
+        
+        return result;
+    }
+    
+    @RequestMapping("/delete")
+    public ReceivePersonMessage deletePerson(@RequestBody PersonDTO person)
+    {
+        /*ReceivePersonMessage result = new ReceivePersonMessage();
+        
+        try
+        {
+            service.(person);
+            result.setObject(person);
+        }
+        catch(Exception ex)
+        {
+            result.setMessage(ex.getMessage());
+            result.setObject(null);
+        }
+        
+        return result;*/
+        
+        //!!!! missing delete method in service layer - ask xstreca1
+        return null;
+    }
+    
+    @RequestMapping("/add")
+    public ReceivePersonMessage addPerson(@RequestBody PersonDTO person)
+    {
+        ReceivePersonMessage result = new ReceivePersonMessage();
+        
+        try
+        {
+            //needs to be edited to return PersonDTO
+            //PersonDTO newPerson = service.createPerson(person);
+            //result.setObject(newPerson);
+        }
+        catch(Exception ex)
+        {
+            result.setMessage(ex.getMessage());
+            result.setObject(null);
+        }
+        
+        return result;
+    }
 }
+
