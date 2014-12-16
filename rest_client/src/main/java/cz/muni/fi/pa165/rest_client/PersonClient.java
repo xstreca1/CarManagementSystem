171d9e023251c59560e2f10ef918f0018e1ee450
xrumanov <jakub.rumanovsky@gmail.com>
2014-12-16 13:40:54 +0100

    implemented the whole PersonClient  *the only goal is to convert Date to String somehow in one attribute

--- rest_client/src/main/java/cz/muni/fi/pa165/rest_client/PersonClient.java
+++ rest_client/src/main/java/cz/muni/fi/pa165/rest_client/PersonClient.java
@@ -2,12 +2,18 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-args
-id name isActive employmentStatus dateOfBirth position sex
-address nationality salary identificationNumber
+ args
+ id name isActive employmentStatus dateOfBirth position sex
+ address nationality salary identificationNumber
  *
  */
 package cz.muni.fi.pa165.rest_client;
+
+import cz.muni.fi.pa165.persistence.Entities.Person;
+import cz.muni.fi.pa165.rest_messages.ReceiveActivePeopleListMessage;
+import cz.muni.fi.pa165.rest_messages.ReceivePersonMessage;
+import cz.muni.fi.pa165.service.dto.PersonDTO;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -25,6 +31,7 @@ public class PersonClient {
 
     private static final String CREATE_PERSON = "http://localhost:8080/pa165/rest/person/add";
     private static final String GET_PERSON = "http://localhost:8080/pa165/rest/person/get?id=%d";
+    private static final String GET_PERSON_NAME = "http://localhost:8080/pa165/rest/person/get?id=%s";
     private static final String UPDATE_PERSON = "http://localhost:8080/pa165/rest/person/update";
     private static final String DELETE_PERSON = "http://localhost:8080/pa165/rest/person/delete";
     private static final String GET_ALL_PEOPLE = "http://localhost:8080/pa165/rest/person/getAll";
@@ -61,7 +68,7 @@ public class PersonClient {
             case FIND_BY_ID_OPERATION:
                 handleFindById(args);
                 break;
-                
+
             case FIND_BY_NAME_OPERATION:
                 handleFindByName(args);
                 break;
@@ -70,31 +77,141 @@ public class PersonClient {
                 Messages.unknownOperationMessage(operation);
                 System.exit(1);
         }
-        
+
     }
 
     private void handleFindAllOperation() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        RestTemplate restTemplate = new RestTemplate();
+        ReceiveActivePeopleListMessage result = restTemplate.postForObject(GET_ALL_PEOPLE, null, ReceiveActivePeopleListMessage.class);
+
+        for (PersonDTO p : result.getList()) {
+            System.out.println(p.toString());
+        }
     }
 
     private void handleAddOperation(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (args.length < 11) {
+            String requiredArgs = "<name> <isActive> <employmentStatus> <dateOfBirth> <position> "
+                    + "<sex> <nationality> <salary> <IdentificationNumber>";
+            Messages.badNumberOfArgsMessage(args.length, ADD_OPERATION, requiredArgs);
+            System.exit(1);
+        }
+
+        PersonDTO person = new PersonDTO();
+        person.setName(args[2]);
+        person.setIsActive(Boolean.valueOf(args[3]));
+        person.setEmploymentStatus(Person.EmploymentStatus.valueOf(args[4]));
+        //convert date to string?
+        person.setDateOfBirth(args[5]);
+        person.setPosition(args[6]);
+        person.setSex(Person.Sex.valueOf(args[7]));
+        person.setNationality(args[8]);
+        person.setSalary(Integer.parseInt(args[9]));
+        person.setIdentificationNumber(args[10]);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ReceivePersonMessage result = restTemplate.postForObject(CREATE_PERSON, person, ReceivePersonMessage.class);
+
+        if (result.isSuccess()) {
+            System.out.println("Person with Identification number: '" + args[10] + "'and name: '" + args[2] + "' was created");
+            return;
+        } else {
+            Messages.serverError(result.getMessage());
+        }
     }
 
     private void handleUpdateOperation(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (args.length < 7) {
+            String requiredArgs = "<id> <name> <position> <nationality> <salary> <employmentStatus> <isActive>";
+            Messages.badNumberOfArgsMessage(args.length, UPDATE_OPERATION, requiredArgs);
+            System.exit(1);
+        }
+
+        PersonDTO person = new PersonDTO();
+        person.setName(args[2]);
+        person.setPosition(args[3]);
+        person.setNationality(args[4]);
+        person.setSalary(Integer.parseInt(args[5]));
+        person.setEmploymentStatus(Person.EmploymentStatus.valueOf(args[6]));
+        person.setIsActive(Boolean.valueOf(args[7]));
+
+        RestTemplate restTemplate = new RestTemplate();
+        ReceivePersonMessage result = restTemplate.postForObject(UPDATE_PERSON, person, ReceivePersonMessage.class);
+
+        if (result.isSuccess()) {
+            System.out.println("Person with id: '" + args[1] + "' was updated");
+        } else {
+            System.out.println("Person with this ID does not exist");
+        }
     }
 
     private void handleDeleteOperation(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (args.length < 3) {
+            String requiredArgs = "<id>";
+            Messages.badNumberOfArgsMessage(args.length, DELETE_OPERATION, requiredArgs);
+            System.exit(1);
+        }
+
+        int id = -1;
+
+        try {
+            id = Integer.parseInt(args[2]);
+        } catch (Exception e) {
+            Messages.serverError("ID - Wrong argument: " + args[2] + ", must be int");
+            return;
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        ReceivePersonMessage result = restTemplate.getForObject(String.format(GET_PERSON, id), ReceivePersonMessage.class);
+
+        if (!result.isSuccess()) {
+            System.out.println("Person with id " + id + " does not exist.");
+            return;
+        }
+
+        ReceivePersonMessage result2 = restTemplate.postForObject(DELETE_PERSON, result.getObject(), ReceivePersonMessage.class);
+
+        if (result2.isSuccess()) {
+            System.out.println("Person with id " + id + " was deleted");
+        } else {
+            Messages.serverError(result.getMessage());
+        }
     }
 
     private void handleFindById(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (args.length < 3) {
+            String requiredArgs = "<id>";
+            Messages.badNumberOfArgsMessage(args.length, FIND_BY_ID_OPERATION, requiredArgs);
+            System.exit(1);
+        }
+        int id = -1;
+        try {
+            id = Integer.parseInt(args[2]);
+        } catch (Exception e) {
+            Messages.serverError("ID - Wrong argument: " + args[2] + ", must be int");
+            return;
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        ReceivePersonMessage result = restTemplate.getForObject(String.format(GET_PERSON, id), ReceivePersonMessage.class);
+
+        PersonDTO person = result.getObject();
+        System.out.println("Person with id " + args[2] + " is person " + person.getName() + " with identification number " + person.getIdentificationNumber());
     }
 
     private void handleFindByName(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        if (args.length < 3) {
+            String requiredArgs = "<name>";
+            Messages.badNumberOfArgsMessage(args.length, FIND_BY_NAME_OPERATION, requiredArgs);
+            System.exit(1);
+        }
+        String name = args[2];
 
+        RestTemplate restTemplate = new RestTemplate();
+        ReceiveActivePeopleListMessage result = restTemplate.getForObject(String.format(GET_PERSON_NAME, name), ReceiveActivePeopleListMessage.class);
+
+        for (PersonDTO p : result.getList()) {
+            System.out.println(p.toString());
+        }
+    }
 }
