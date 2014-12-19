2f519b3d4cf417fe5b4174a525dd33461d65fe3c
xrumanov <jakub.rumanovsky@gmail.com>
2014-12-19 10:17:50 +0100

    clean car client and fix errors

--- rest_client/src/main/java/cz/muni/fi/pa165/rest_client/CarClient.java
+++ rest_client/src/main/java/cz/muni/fi/pa165/rest_client/CarClient.java
@@ -86,46 +86,48 @@ public class CarClient {
     }
 
     /**
-     * handles create car operation console command is: car create
-     * [vehicleRegPlate] [brand] [typeName] [VIN] [yearOfManufacture] 
-     * [color] [mileage] [bodystyle] [enginePower] [gasConsumption] [transmission]
-     * [emissionstandard] [numberOfSeats] [category] [isActive]
+     * handles create car operation 
+     * console command is: 
+     * car create [vehicleRegPlate] [brand] [typeName] [VIN] [yearOfManufacture] [bodystyle]
+     * [numberOfSeats] [mileage] [color] [category] [emissionstandard]
+     * [availibility] [isActive]
      *
      * @param args command line arguments args[0] args[1] args[2] args[3]
-     * args[4] args[5] args[6] args[7] args[8] args[9] args[10] args[11] args[12] args[13]
-     * 
+     * args[4] args[5] args[6] args[7] args[8] args[9] args[10] args[11]
+     * args[12] args[13]
+     *
      */
     private void handleAddOperation(String args[]) {
-        if (args.length < 14) {
-            String requiredArgs = "<vehicleRegPlate> <brand> <typeName> <VIN> <yearOfManufacture> "
-                    + "<color> <mileage> <bodystyle> <enginePower> <gasConsumption> <transmission> "
-                    + "<emissionstandard> <numberOfSeats> <category> <isActive>";
+        if (args.length < 15) {
+            String requiredArgs = "[vehicleRegPlate] [brand] [typeName] "
+                    + "[VIN] [yearOfManufacture] [bodystyle] [numberOfSeats] "
+                    + "[mileage] [color] [category] [emissionstandard]"
+                    + "[availibility] [isActive]";
+
             Messages.badNumberOfArgsMessage(args.length, ADD_OPERATION, requiredArgs);
             System.exit(1);
         }
-        
+
         CarDTO car = new CarDTO();
         car.setVehicleRegPlate(args[2]);
         car.setBrand(args[3]);
         car.setTypeName(args[4]);
         car.setVIN(args[5]);
         car.setYearOfManufacture(Integer.parseInt(args[6]));
-        car.setColor(Car.Color.valueOf(args[7]));
-        car.setMileage(Integer.parseInt(args[8]));
-        car.setBodystyle(Car.bodyStyle.valueOf(args[9]));
-        car.setEnginePower(Integer.parseInt(args[10]));
-        car.setGasConsumption(Float.parseFloat(args[11]));
-        car.setTransmission(Boolean.parseBoolean(args[12]));
-        car.setEmissionstandard(Car.emissionStandard.valueOf(args[13]));
-        car.setNumberOfSeats(Integer.parseInt(args[14]));
-        car.setCategory(Car.Category.valueOf(args[15]));
-        car.setIsActive(Boolean.parseBoolean(args[16]));
+        car.setBodystyle(Car.bodyStyle.valueOf(args[7]));
+        car.setNumberOfSeats(Integer.parseInt(args[8]));
+        car.setMileage(Integer.parseInt(args[9]));
+        car.setColor(Car.Color.valueOf(args[10]));
+        car.setCategory(Car.Category.valueOf(args[11]));
+        car.setEmissionstandard(Car.emissionStandard.valueOf(args[12]));
+        car.setAvailibility(Boolean.parseBoolean(args[13]));
+        car.setIsActive(Boolean.parseBoolean(args[14]));
 
         RestTemplate restTemplate = new RestTemplate();
         ReceiveCarMessage result = restTemplate.postForObject(CREATE_CAR, car, ReceiveCarMessage.class);
 
         if (result.isSuccess()) {
-            System.out.println("Car with RegistrationNumber: '" + args[2] + "', type: '" + args[3] + "', model: '" + args[4] + "', vin: '" + args[5] + "' was created");
+            System.out.println("Car with registration plate: '" + args[2] + "', brand: '" + args[3] + "', model: '" + args[4] + "', vin: '" + args[5] + "' was created");
             return;
         } else {
             Messages.serverError(result.getMessage());
@@ -133,7 +135,8 @@ public class CarClient {
     }
 
     /**
-     * find carits id
+     * find car by its id
+     * command is: car findbyid [id]
      *
      * @param args car
      */
@@ -155,20 +158,20 @@ public class CarClient {
         ReceiveCarMessage result = restTemplate.getForObject(String.format(GET_CAR, id), ReceiveCarMessage.class);
 
         CarDTO car = result.getObject();
-        System.out.println("Car with id " + args[2] + " is car " + car.getBrand()+ " " + car.getTypeName()+ " with registration number " + car.getVehicleRegPlate());
+        System.out.println("Car with id " + args[2] + " is car " + car.getBrand() + " " + car.getTypeName() + " with registration number " + car.getVehicleRegPlate());
     }
 
     /**
-     * handles update carration console command is: car update [carID]
-     * [mileage] [availability] [isActive]
+     * handles update car operation 
+     * console command is: car update [carID] [mileage] [availability] [isActive]
      *
      * @param args command line arguments args[0] args[1] args[2] args[3]
-     * args[4] args[5] args[6] args[7] args[8] car create id registrationNumber
-     * type model vin fuel color
+     * args[4] args[5] 
+     * car create id mileage availability isActive
      */
     private void handleUpdateOperation(String[] args) {
-        if (args.length < 4) {
-            String requiredArgs = "<id> <mileage> <availability> <isActive>";
+        if (args.length < 6) {
+            String requiredArgs = "[id] [mileage] [availability] [isActive]";
             Messages.badNumberOfArgsMessage(args.length, UPDATE_OPERATION, requiredArgs);
             System.exit(1);
         }
@@ -190,13 +193,15 @@ public class CarClient {
     }
 
     /**
-     * handles removal of car entity, command name 'delete'
+     * handles removal of car entity
+     * console command is 'car delete id'
+     * it only deactivates car
      *
      * @param args command line arguments args[0] args[1] args[2] car delete id
      */
     private void handleDeleteOperation(String[] args) {
         if (args.length < 3) {
-            String requiredArgs = "<id>";
+            String requiredArgs = "[id]";
             Messages.badNumberOfArgsMessage(args.length, DELETE_OPERATION, requiredArgs);
             System.exit(1);
         }
