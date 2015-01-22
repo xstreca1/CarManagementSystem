10324b062adb08e18380242cfa611625f0387989
xrumanov <jakub.rumanovsky@gmail.com>
2015-01-22 14:02:28 +0100

    created test page mainPage to see if login works finished controller and loginPage for auth

--- web/src/main/java/cz/muni/fi/pa165/web/LoginController.java
+++ web/src/main/java/cz/muni/fi/pa165/web/LoginController.java
@@ -9,6 +9,36 @@ package cz.muni.fi.pa165.web;
  *
  * @author jrumanov
  */
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class LoginController {
-    
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal) {
+
+        String name = principal.getName();
+        model.addAttribute("username", name);
+        return "mainPage";
+
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+
+        return "loginPage";
+
+    }
+
+    @RequestMapping(value = "/loginError", method = RequestMethod.GET)
+    public String loginError(ModelMap model) {
+        model.addAttribute("error", "true");
+        return "loginPage";
+
+    }
+
 }
